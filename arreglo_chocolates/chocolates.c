d1ad7be3304378ddfc91f100340bb9455f0298ce
Luis Enrique Canseco Gutiérrez <lcanseco.enrique@gmail.com>
2016-11-22 15:24:43 -0600

    arreglo chocolates
    /* Crear un programa que solicite el valor del arreglo y que
    sume chocolates mediante un arreglo. */

---
+++ arreglo_chocolates/chocolates.c
@@ -0,0 +1,24 @@
+/* Crear un programa que solicite el valor del arreglo y que
+sume chocolates mediante un arreglo. */
+
+#include<cstdlib>
+#include<iostream>
+using namespace std;
+main ()
+{
+
+         int chocolates [10];
+         int i;
+         int suma=0;
+         for(i=0;i<10;i++)
+         cout<<"\n ingrese un valor para el arreglo";
+         cin>>chocolates[i];
+         suma=suma+chocolates[i];
+         }
+
+         cout<<"\n\n La suma de los chocolates es"<<suma;
+         cout<<"\n\n\n";
+
+system ("PAUSE");
+return 0;
+}
